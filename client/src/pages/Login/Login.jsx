4a7a73e02e4017da496887ae9ec56adf031c2702
Yakimowv <yakimowv@gmail.com>
2022-12-07 14:45:56 +0200

    Make Login HTML and SCSS

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 import './Login.scss'
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+	return (
+		<div className='login'>
+			<div className='card'>
+				<div className='left'>
+					<h1>Hello World.</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+						alias totam numquam ipsa exercitationem dignissimos, error nam,
+						consequatur.
+					</p>
+					<span>Don;t have an account?</span>
+					<button>Register</button>
+				</div>
+				<div className='right'>
+					<h1>Login</h1>
+					<form>
+						<input type="text" placeholder='Username' />
+						<input type="password" placeholder='Password' />
+						<button>Login</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
 }
