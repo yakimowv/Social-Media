498b92a7dcb9a3837d6b741ad206487f66f09dc9
yakimowv <100320246+yakimowv@users.noreply.github.com>
2022-12-12 12:05:02 +0200

    Make RightBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,64 @@
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import Home from './pages/Home/Home'
-import Register from './pages/Register/Register';
-import Profile from './pages/Profile/Profile';
-import Navbar from './components/Navbar/Navbar';
-import Leftbar from './components/Leftbar/Leftbar';
-import Rightbar from './components/Rightbar/Rightbar';
-import { ProtectRoute } from './guards/ProtectRoute'
-
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import Profile from "./pages/Profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
+import Leftbar from "./components/Leftbar/Leftbar";
+import Rightbar from "./components/Rightbar/Rightbar";
+import { ProtectRoute } from "./guards/ProtectRoute";
+import './style.scss'
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="theme-dark">
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <Leftbar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <Rightbar />
+        </div>
+      </div>
+    );
+  };
 
-	const Layout = () => {
-		return (
-			<div>
-				<Navbar />
-				<div style={{ display: 'flex' }}>
-					<Leftbar />
-					<div style={{ flex: 6 }}>
-						<Outlet />
-					</div>
-					<Rightbar />
-				</div>
-			</div>
-		)
-	}
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectRoute>
+          <Layout />
+        </ProtectRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "register",
+      element: <Register />,
+    },
+  ]);
 
-
-
-	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: (
-				<ProtectRoute>
-					<Layout />
-				</ProtectRoute>
-			),
-			children: [
-				{
-					path: '/',
-					element: <Home />
-				},
-				{
-					path: '/profile/:id',
-					element: <Profile />
-				}
-			]
-		},
-		{
-			path: "/login",
-			element: <Login />
-		},
-		{
-			path: "register",
-			element: <Register />
-		}
-	])
-
-
-
-	return (
-		<div>
-			<RouterProvider router={router} />
-		</div>
-	)
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Rightbar/Rightbar.jsx
+++ client/src/components/Rightbar/Rightbar.jsx
@@ -1,7 +1,108 @@
 import './Rightbar.scss'
+import SuggestionPicture from '../../assets/asd.png'
+import SuggestionPictureSecond from '../../assets/dddd.jpg'
+import SuggestionPictureFourth from '../../assets/a1.jpg'
+import SuggestionPictureThird from '../../assets/a2.jpg'
 
 export default function Rightbar() {
-  return (
-    <div className='rightbar'>Rightbar</div>
-  )
+	return (
+		<div className='rightbar'>
+			<div className='container'>
+				<div className='item'>
+					<span>Suggestions For You</span>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPicture} alt='User' />
+							<span>Natsu Dragneel</span>
+						</div>
+						<div className='buttons'>
+							<button>follow</button>
+							<button>dismiss</button>
+						</div>
+					</div>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPictureSecond} alt='User' />
+							<span>Itachi Uchiha</span>
+						</div>
+						<div className='buttons'>
+							<button>follow</button>
+							<button>dismiss</button>
+						</div>
+					</div>
+				</div>
+				<div className="item">
+					<span>Latest Activities</span>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPicture} alt='User' />
+							<p>
+								<span>Natsu Dragneel</span> changed their cover picture
+							</p>
+						</div>
+						<span>1 mi ago</span>
+					</div>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPictureSecond} alt='User' />
+							<p>
+								<span>Itachi Uchiha</span> changed their cover picture
+							</p>
+						</div>
+						<span>10 mi ago</span>
+					</div>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPictureThird} alt='User' />
+							<p>
+								<span>Naruto Uzumaki</span> changed their cover picture
+							</p>
+						</div>
+						<span>1 mi ago</span>
+					</div>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPictureFourth} alt='User' />
+							<p>
+								<span>Saske Uchiha</span> changed their cover picture
+							</p>
+						</div>
+						<span>1 mi ago</span>
+					</div>
+				</div>
+				<div className="item">
+					<span>Online Friends</span>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPictureFourth} alt='User' />
+							<div className='online'/>
+								<span>Saske Uchiha</span> 
+						</div>
+					</div>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPictureSecond} alt='User' />
+							<div className='online'/>
+								<span>Itachi Uchiha</span> 
+						</div>
+					</div>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPictureThird} alt='User' />
+							<div className='online'/>
+								<span>Naruto Uzumaki</span> 
+						</div>
+					</div>
+					<div className='user'>
+						<div className='userInfo'>
+							<img src={SuggestionPicture} alt='User' />
+							<div className='online'/>
+								<span>Natsu Dragniel</span> 
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	)
 }
